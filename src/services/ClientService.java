5074a57e07b4d01240a5f4bcfd370369989b0a5a
Rayan <rayanpetros2@gmail.com>
2024-01-15 08:55:28 -0300

    feat: adding defensive programming techiques

--- src/services/ClientService.java
+++ src/services/ClientService.java
@@ -2,6 +2,7 @@ package services;
 
 import domain.Client;
 import dto.ClientReportDTO;
+import dto.EntityDTO;
 import enums.FilteringOption;
 import exceptions.ClientException;
 import repository.classes.ClientRepository;
@@ -19,13 +20,13 @@ public class ClientService {
 
     public Client createClient() {
 
-        String name = ReadService.readString("Enter the client name: ");
+        String name = ReadService.readString("Enter the client name");
         if (!validString(name)) throw new ClientException("Invalid name!");
 
-        int age = ReadService.readInt("Enter the client age : ");
+        int age = ReadService.readInt("Enter the client age");
         if (!validAge(age)) throw new ClientException("Invalid age!");
 
-        String city = ReadService.readString("Enter the client city: ");
+        String city = ReadService.readString("Enter the client city");
         if (!validString(city)) throw new ClientException("Invalid city!");
 
         return Client.ClientBuilder.builder()
@@ -36,15 +37,6 @@ public class ClientService {
                 .build();
     }
 
-    public ClientReportDTO createClientDTO(Client client) {
-        return ClientReportDTO.ClientReportDTOBuilder.builder()
-                .name(client.getName())
-                .age(client.getAge())
-                .creationDate(client.getCreationInstant())
-                .index(clientRepository.findIndex(client))
-                .build();
-    }
-
 
     public boolean saveClient(Client client) {
         return clientRepository.save(client);
@@ -52,16 +44,29 @@ public class ClientService {
 
     public Optional<Client> findClient() {
 
-        FilteringOption filteringOption = ReadService.readOption(FilteringOption.class);
+        FilteringOption filteringOption = ReadService.readEnum(FilteringOption.class);
+
+        //Adding defensive programming
+        if (filteringOption == null) throw new ClientException("Filtering option cannot be null!");
 
         return switch (filteringOption) {
             case BY_NAME -> {
                 String name = ReadService.readString("Enter the client name: ");
+
+                //Adding defensive programming
+                if (name == null) throw new ClientException("Name cannot be null!");
                 yield clientRepository.findByName(name);
             }
 
             case LAST_ADDED -> clientRepository.findLastAdedd();
+
+            //Adding defensive programming
+            default -> throw new IllegalArgumentException("This option is deprecated!");
         };
+    }
+
+    public EntityDTO createClientDTO(Client client) {
+        return new ClientReportDTO(client, clientRepository.findIndex(client));
     }
 
     private static boolean validString(String str) {
