504aae661727c6a9a976e6b364017e1ce5d3849c
Rayan <rayanpetros2@gmail.com>
2024-01-15 08:54:42 -0300

    feat: add usused enum for defensive programming example

--- src/enums/FilteringOption.java
+++ src/enums/FilteringOption.java
@@ -1,7 +1,8 @@
 package enums;
 
 public enum FilteringOption {
-    LAST_ADDED, BY_NAME;
+                        //Unused option, created for defensive programming in switch return example
+    LAST_ADDED, BY_NAME, OTHER_OPTION;
 
 
 }
